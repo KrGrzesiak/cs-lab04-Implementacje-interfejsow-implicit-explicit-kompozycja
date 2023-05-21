f8aea2a12447d17c6c9c5556aec333ce2af65134
KrGDarkSide <jul.mdama08@gmail.com>
2023-05-21 13:47:04 +0200

    fax

---
+++ Copier/Zadanie2/Fax.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ver1
+{
+    public interface IFax : IDevice
+    {
+        void Send(in IDocument document);
+    }
+}
